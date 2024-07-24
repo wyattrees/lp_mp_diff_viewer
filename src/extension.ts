7a4911647cd8c74a95c70c789f9deb54f0cb74c7
Wyatt Rees <wyatt.rees@canonical.com>
2024-07-24 16:21:55 -0600

    Do not clone new repos in tmp, just add remotes, generate a diff, and apply it

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,9 @@ const got = require('got');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+const TARGET_REMOTE = "lp-diff-target"
+const SOURCE_REMOTE = "lp-diff-src"
+
 type MergeProposal = {
 	target_branch: string;
 	source_branch: string;
@@ -21,52 +24,29 @@ const openInDiffEditor = (leftFilePath: string, rightFilePath: string, title: st
 };
 
 
-async function clone_repos(lp_username: string, mp: MergeProposal): Promise<string[]> {
-	cp.exec("mkdir -p tmp/source", (err: Error, stdout: string, stderr: string) => {
-		console.log(err);
-		console.log(stdout);
-		console.log(stderr);
-	});
-	cp.exec("mkdir -p tmp/target", (err: Error, stdout: string, stderr: string) => {
-		console.log(err);
-		console.log(stdout);
-		console.log(stderr);
-	});
-	cp.exec("git clone --depth 1 -b " + mp.source_branch + " git+ssh://" + lp_username + "@git.launchpad.net/" + mp.source_repo + " tmp/source", (err: Error, stdout: string, stderr: string) => {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (err) {
-			console.log('error: ' + err);
-		}
-	});
-	// get the upstream and diff
-	cp.exec("cd tmp/source/ && git add remote upstream git+ssh://" + lp_username + "@git.launchpad.net/" + mp.target_repo + " && git remote update upstream", (err: Error, stdout: string, stderr: string) => {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (err) {
-			console.log('error: ' + err);
-		}
-	});
-	cp.exec("git clone --depth 1 -b " + mp.target_branch + " git+ssh://" + lp_username + "@git.launchpad.net/" + mp.target_repo + " tmp/source", (err: Error, stdout: string, stderr: string) => {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (err) {
-			console.log('error: ' + err);
-		}
-	});
-	return new Promise<string[]>(function (resolve, reject) {
-		cp.exec("cd tmp/source && git diff upstream/" + mp.target_branch + " --name-only", (err: Error, stdout: string, stderr: string) => {
-			if (err) {
-				console.log('error: ' + err);
-				reject("Failed to get list of files changed")
-			}
-			else {
-				resolve(stdout.split("\n"))
-			}
-		});
-	})
+function get_diff(lp_username: string, mp: MergeProposal): string[] {
+	cp.execSync("git remote add " + TARGET_REMOTE + " git+ssh://" + lp_username + "@git.launchpad.net/" + mp.target_repo + " && git remote update " + TARGET_REMOTE);
+	cp.execSync("git remote add " + SOURCE_REMOTE + " git+ssh://" + lp_username + "@git.launchpad.net/" + mp.source_repo + " && git remote update " + SOURCE_REMOTE);
+	cp.execSync("git checkout " + SOURCE_REMOTE + "/" + mp.source_branch);
+	var files: string[] = cp.execSync("git diff " + TARGET_REMOTE + "/" + mp.target_branch + " --name-only").split("\n");
+	cp.execSync("git diff " + TARGET_REMOTE + "/" + mp.target_branch + " > diff.patch");
+	return files
 }
 
+
+function make_tmp_files_and_patch(files: string[]): void {
+	files.forEach(f => {
+		cp.execSync("cp " + f + " " + f + ".orig");
+	});
+	cp.execSync("git apply diff.patch");
+}
+
+function cleanup(lp_username: string, mp: MergeProposal, files: string[]): void {
+	files.forEach(f => {
+		cp.execSync("rm " + f + ".orig")
+	})
+
+}
 
 async function get_repository_urls(lp_mp_url: string): Promise<MergeProposal> {
 	return new Promise<MergeProposal>(function (resolve, reject) {
@@ -95,9 +75,11 @@ const diff_lp = async (): Promise<void> => {
 	var mp: MergeProposal;
 	mp = await get_repository_urls(lp_url);
 	console.log(mp)
-	var files_changed: string[] = await clone_repos(lp_username, mp);
-
-	openInDiffEditor("/home/wyatt/lp-diff-viewer/test1.txt", "/home/wyatt/lp-diff-viewer/test2.txt", "Test diff");
+	var files_changed: string[] = get_diff(lp_username, mp);
+	console.log(files_changed)
+	make_tmp_files_and_patch(files_changed);
+	openInDiffEditor(files_changed[0], files_changed[0] + ".orig", "test");
+	// cleanup(lp_username, mp, files_changed);
 }
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
